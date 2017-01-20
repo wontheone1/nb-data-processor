9e2bbde2969db8b07d5f651acf8265729667c1b4
WON SEOB SEO <pghoou@gmail.com>
2017-01-21 00:19:55 +0200

    Enable remove space bar possible

--- src/nb_mart/csv.clj
+++ src/nb_mart/csv.clj
@@ -70,8 +70,8 @@
          (sort-by count >))))
 (defn lookup-standard-name [standard-names non-standard-name]
   (some #(if (.startsWith non-standard-name %) %) standard-names))
-(defn insert-standardized-model-names [model-file-path whole-sale-order-file-path]
-  (let [whole-sale-data (read-csv-without-bom whole-sale-order-file-path)]
+(defn insert-standardized-model-names [model-file-path whole-sale-order-file-path separator]
+  (let [whole-sale-data (read-csv-without-bom whole-sale-order-file-path separator)]
     (for [a-row whole-sale-data]
       (let [standard-names    (read-standard-model-names model-file-path)
             standard-name     (or (lookup-standard-name standard-names (a-row 2))
@@ -116,9 +116,9 @@
           remove-space-bar)
       (re-find all-caps-model-name-matcher string)
       (re-find hangeul-matcher string)))
-(defn insert-model-names-from-csv-file [sabang-file-path]
+(defn insert-model-names-from-csv-file [sabang-file-path separator]
   "Returns a lazy sequence of vectors with model names as the first element"
-  (let [sabang-net-data (read-csv-without-bom sabang-file-path)]
+  (let [sabang-net-data (read-csv-without-bom sabang-file-path separator)]
     (for [a-row sabang-net-data]
       (if (blank? (a-row 0))
         (assoc a-row 0 (string->model-name (str (a-row 1) " " (a-row 3))))
@@ -130,19 +130,19 @@
     (let [model-name (a-row 0)
           partner-name (model->partner model-name)]
       (concat [partner-name] a-row))))
-(defn process-sabang-data [model-partner-file-path sabang-data-file-path]
+(defn process-sabang-data [model-partner-file-path sabang-data-file-path separator]
   "Process data from sabangnet, write to a file, return the file path"
   (let [model->partner (read-model->partner model-partner-file-path)
-        sabang-data    (insert-model-names-from-csv-file sabang-data-file-path)]
+        sabang-data    (insert-model-names-from-csv-file sabang-data-file-path separator)]
     (insert-partner-names model->partner sabang-data)))
-(defn generate-processed-csv! [model-partner-file-path data-file-path data-type]
+(defn generate-processed-csv! [model-partner-file-path data-file-path data-type separator]
   (-> (case data-type
-        :sabangnet (process-sabang-data model-partner-file-path data-file-path)
-        :whole-sale (insert-standardized-model-names model-partner-file-path data-file-path))
-      write-processed-data-to-file!))
+        :sabangnet (process-sabang-data model-partner-file-path data-file-path separator)
+        :whole-sale (insert-standardized-model-names model-partner-file-path data-file-path separator))
+      (write-processed-data-to-file! separator)))
 
 ;; To deal with file uploads
-(defn file-uploads-then-return-result! [model-file data-csv-file data-type]
+(defn file-uploads-then-return-result! [model-file data-csv-file data-type separator]
   (let [model-file-path  (create-temp-file! model-file)
         data-csv-file-path (create-temp-file! data-csv-file)]
-    (generate-processed-csv! model-file-path data-csv-file-path data-type)))
+    (generate-processed-csv! model-file-path data-csv-file-path data-type (first (char-array separator)))))
