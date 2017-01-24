678e6f57484972d0500b502f802a4dfee08bd5d4
WON SEOB SEO <pghoou@gmail.com>
2017-01-24 22:07:36 +0200

    Fix matcher

--- src/nb_mart/matcher.clj
+++ src/nb_mart/matcher.clj
@@ -2,7 +2,7 @@
 
 (def chumy-underscore-model-name-matcher #"[A-Z0-9]+_[0-9]+")
 (def byc-eng-num-slash-only-matcher #"BY[a-zA-Z]*[0-9]+(\/[0-9]+)*")
-(def eng-num-dash-matcher #"[0-9]*[a-zA-Z]{2,}-?[0-9]+[a-zA-Z0-9-]+(\/[0-9]*[a-zA-Z]*-?[0-9]+[a-zA-Z0-9-]+)*")
+(def eng-num-dash-matcher #"[a-zA-Z0-9]*[a-zA-Z]{2,}-?[0-9]+[a-zA-Z0-9-]+(\/[a-zA-Z0-9]*[a-zA-Z]*-?[0-9]+[a-zA-Z0-9-]+)*")
 (def eng-num-space-matcher #"[a-zA-Z]+ ?[0-9]+[a-zA-Z0-9]*(\/[a-zA-Z]* ?[0-9]+[a-zA-Z0-9]*)*")
 (def freebies-matcher #".*\(사은품\).*")
 (def all-caps-model-name-matcher #"[A-Z]{4,}")
